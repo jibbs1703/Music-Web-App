907704e8bee03e8ec4090fab156c5b16132054e7
Abraham Olakunle Ajibade <abrahamjibbs1703@gmail.com>
2024-04-12 13:19:10 -0400

    Created functions to transform results from extraction phase

--- SmartAppTransform.py
+++ SmartAppTransform.py
@@ -13,31 +13,38 @@ types = []
 
 columns = {}
 
-def flatten(data):
+def flatten(data, append=None):
     if type(data) == dict:
         
         for key in data.keys():
             if type(data[key]) in [str, int, None]:
                 if key == 'href':
                     href.append(data[key])
+                    columns[key] = href
                 elif key == 'id':
                     ids.append(data[key])
+                    columns[key] = ids
                 elif key == 'name':
                     name.append(data[key])
+                    columns[key] = name
                 elif key == 'type':
                     types.append(data[key])
+                    columns[key] = types                    
                 elif key == 'popularity':
-                    popularity.append(data[key]) 
+                    popularity.append(data[key])
+                    columns[key] = popularity 
                 elif key == 'spotify':
-                        spotify.append(data[key])
+                    spotify.append(data[key])
+                    columns[key] = spotify   
                 elif key == 'total':
-                    total.append(data[key])               
+                    total.append(data[key])
+                    columns[key] = total                    
             
             elif type(data[key]) == list:
                 if sum([True for k in data[key] if type(k) == str]) == len(data[key]):
-                    genre.append(data[key])
-                else:
-                     flatten(data[key])
+                    if key == 'genres':
+                        genre.append(data[key])
+                        columns[key] = genre
             else: 
                 flatten(data[key])
    
@@ -47,3 +54,5 @@ def flatten(data):
 
     else:
         print(f"{data} cannot be transformed at this time")
+
+    return columns
